70b9afa98164641d7de9f7b7c1da602703373a23
[nick_fasulo] <fasulo.nick@gmail.com>
2021-08-04 14:44:20 -0400

    working on all prodcuts view

--- client/index.js
+++ client/index.js
@@ -5,15 +5,12 @@ import { Router } from 'react-router-dom';
 import history from './history';
 import store from './store';
 import App from './App';
-import AllProducts from "./components/AllProducts";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <AllProducts />
+      <App />
     </Router>
   </Provider>,
   document.getElementById('app')
 );
-
-//<App /> 
